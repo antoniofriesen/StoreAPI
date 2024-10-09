ea9f75293b3f0f5dd4ce681997ad0285373edebb
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-09 13:08:31 +0200

    DB and rooutes
    connection to DB stablished and first routes added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config;
-// async errors
+require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const APP = express();
-
-const notFoundMiddleware = require("./middleware/not-found.js");
-const errorMiddleware = require("./middleware/error-handler.js");
-const notFound = require("./middleware/not-found.js");
+const connectDB = require("./db/connect.js");
+const productsRouter = require("./routes/products.js");
+const errorMiddleware = require("./middleware/errorHandler.js");
+const notFound = require("./middleware/notFound.js");
 
 // middleware
 APP.use(express.json());
@@ -14,6 +14,9 @@ APP.use(express.json());
 APP.get("/", (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+APP.use("/api/v1/products", productsRouter);
+
 // products route
 APP.use(notFound);
 APP.use(errorMiddleware);
@@ -24,6 +27,11 @@ const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         // connectDB
+        await connectDB(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("CONNECTED TO DB!");;
         APP.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error) {
         console.log(error);
