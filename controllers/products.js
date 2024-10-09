a442424e63e0f9d90f6283ebd2fd9c584bc4b273
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-09 14:10:47 +0200

    Schema
    Schema for the products added

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,8 @@
+const Product = require("../models/product.js");
 
 const getAllProductsStatic = async (req, res) => {
-    // throw new Error("testing async errors");
-    res.status(200).json({ msg: "products testign route" });
+    const products = await Product.find({ featured: true });
+    res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
