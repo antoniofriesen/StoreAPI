d98c4b0a5e30c2cab2f2ddb430036fe474fbba0b
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-21 12:28:18 +0200

    query features
    query features (sort, skip, ...) implemented

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getAllProducts, getAllProductsStatic } = require("../controllers/products.js");
 
 router.route("/").get(getAllProducts);
-router.route("/static").get(getAllProductsStatic);
+router.route("/static").get(getAllProductsStatic); // Just for manually testing the features
 
 module.exports = router;
